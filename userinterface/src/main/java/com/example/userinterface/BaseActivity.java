f6d9a38571f466c890ead52f2a38c3c4da9145f8
githubuuuuu <1578486963@qq.com>
2018-01-24 20:39:02 +0800

    2018/01/24, 20:38 , githubuuuuu commit.

---
+++ userinterface/src/main/java/com/example/userinterface/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.userinterface;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by WJ on 2018-01-24.
+ *
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ userinterface/src/main/java/com/example/userinterface/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.userinterface;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
