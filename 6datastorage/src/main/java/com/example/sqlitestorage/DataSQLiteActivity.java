d98e784a8a483722103058be86de35d971097c41
githubjjjjj <1723982816@qq.com>
2018-01-24 11:06:43 +0800

    2018/01/24,11:06,githubwwwww Commit

--- 6datastorage/src/main/java/com/example/sqlitestorage/DataSQLiteActivity.java
+++ 6datastorage/src/main/java/com/example/sqlitestorage/DataSQLiteActivity.java
@@ -1,7 +1,10 @@
 package com.example.sqlitestorage;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -41,12 +44,26 @@ public class DataSQLiteActivity extends BaseActivity {
 
             }
         });
-
-        Button createData = (Button) findViewById(R.id.Button_DS3);
-        createData.setOnClickListener(new View.OnClickListener(){
+        // Insert data to Database table
+        Button insertData = (Button) findViewById(R.id.Button_DS3);
+        insertData.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                // Start assembly first data.
+                values.put("name", "The Frist Line code");
+                values.put("author", "GuoLin");
+                values.put("price", 40.00);
+                values.put("pages", 300);
+                db.insert("Book", null, values);
+                values.clear();
+                // assembly second data.
+                values.put("name", "JAVA 入门");
+                values.put("author", "xxxx.xxx");
+                values.put("price", 50.00);
+                values.put("pages", 400);
+                db.insert("Book", null, values);
             }
         });
 
@@ -54,15 +71,26 @@ public class DataSQLiteActivity extends BaseActivity {
         updateData.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                // data
+                values.put("name", "The First Line Code");
+                db.update("Book", values, "price = ?", new String[]{"40.00"});
+                values.clear();
+                // data 2
+                values.put("name", "JAVA 入门");
+                db.update("Book", values, "author = ?", new String[]{"xxxx.xxx"});
             }
         });
 
+        // Delete Data from Table "Book".
         Button deleteData = (Button) findViewById(R.id.Button_DS5);
         deleteData.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                // 删除超过380页的书籍数据. "pages > ?"
+                db.delete("Book", "pages > ?", new String[]{"380"});
             }
         });
 
@@ -70,7 +98,26 @@ public class DataSQLiteActivity extends BaseActivity {
         retrieveData.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                // step 1 :
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                // step 2 :
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                // step 3 :
+                if (cursor.moveToFirst()) {
+                    do {
+                        int id = cursor.getInt(cursor.getColumnIndex("id"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        Log.d("DataSQLiteActivity", "Book id is     " + id);
+                        Log.d("DataSQLiteActivity", "Book author is " + author);
+                        Log.d("DataSQLiteActivity", "Book price is  " + price);
+                        Log.d("DataSQLiteActivity", "Book pages is  " + pages);
+                        Log.d("DataSQLiteActivity", "Book name is   " + name);
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
 
